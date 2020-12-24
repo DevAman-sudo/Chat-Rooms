75848d6b5d09686e7a97d8ba51716559e4f2745a
DevAman-sudo <devaman.net@gmail.com>
2020-12-24 23:05:29 +0545

    Working On Client Side

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,19 @@ const socket = io();
 
 // JavaScript DOM Elements //
 const dataContainer = document.getElementById('data-container');
+const form = document.getElementById('form');
+const textArea = document.getElementById('text-area');
+const  button = document.getElementById('button');
+
+// Listining Submit Event On Form //
+form.addEventListener( 'submit' , (event) => {
+	// Preventing Page From Submitting //
+	event.preventDefault();
+	// Creating Elements To Store Data //
+	// createSmallElement();
+	// console.log(x);
+});
+
+const createSmallElement = () => {
+	let x = 0 ;
+}
