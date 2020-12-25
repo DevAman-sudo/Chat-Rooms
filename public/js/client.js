f5066a4d5f03ee0c7ebf0e7af67721579791c9c3
DevAman-sudo <devaman.net@gmail.com>
2020-12-25 21:53:30 +0545

    Client Side Coding

--- public/js/client.js
+++ public/js/client.js
@@ -2,15 +2,11 @@
 const socket = io();
 
 // Fetching User Name //
-const promptName = prompt("Enter Your Name => ");
+const username = prompt("Enter Your Name => ");
 
 // Global Variables //
-var name;
+// var name;
 
 // Web Socket | Socket.Io Events //
-socket.emit( 'fetch-name' , promptName);
-socket.on('broadcast-name' , (username) => {
-    console.log(`dndnd    ${username}`);
-    username === name ;
-});
+socket.emit( 'fetch-name' , username);
 console.log(name);
