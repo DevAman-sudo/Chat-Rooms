d717c738ecf92ac5761c6be0ba33aeab0241db6f
DevAman-sudo <devaman.net@gmail.com>
2020-12-26 15:40:10 +0545

    Broadcasted User Name On Web Page

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // Socket Liabrary //
-// const socket = io();
+const socket = io();
 
 // Prompt UserName //
 const username = prompt('Enter Your Name => ');
@@ -12,39 +12,33 @@ const button = document.getElementById('button');
 
 // Listening An Event On Form Submit //
 form.addEventListener('submit', (event) => {
-	// Preventing Page To Submit //
-	event.preventDefault();
+    // Preventing Page To Submit //
+    event.preventDefault();
 
-	// Calling Function Create Element //
-	CreateElement(textArea.value);
+    // Calling Function Append Data //
+    // appendData(element, 'middle-msg');
 
-	// Calling Function Append Data //
-	appendData(element, 'middle-msg');
-
-	// Clearing Text Area After Submit //
-	textArea.value = "";
+    // Clearing Text Area After Submit //
+    textArea.value = "";
 });
 
 // Function To Create Element //
 function CreateElement(data) {
-	element = document.createElement('small');
-	element.classList.add('msg');
-	element.innerText = data;
+    
 }
 
 // Function To Append Data //
 function appendData(data, className) {
-	data.classList.add(className);
-	dataContainer.append(data);
+    element = document.createElement('small');
+    element.classList.add('msg' , className);
+    element.innerText = data;
+    dataContainer.append(element);
 }
 
 // Web Socket Connection || socket.io //
-io.on( 'connection' , (socket) => {
-	
-	// new user joined event //
-	socket.emit('new-user-joined' , username);
-	socket.on('user-joined' , (username) => {
-		appendData(username , 'middle-msg')
-	});
-	
+
+// new user joined event //
+socket.emit('new-user-joined', username);
+socket.on('user-joined', (username) => {
+    appendData(`${username} Joined The Chat`, 'middle-msg');
 });
